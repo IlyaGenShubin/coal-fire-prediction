869406070d109642c869023e1c4a3d36ca548166
IlyaGenShubin <166981604+IlyaGenShubin@users.noreply.github.com>
2025-11-24 11:02:08 +0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,5 +3,8 @@ from app.services.predictor import train_and_save_model
 
 def ensure_model_trained():
     if not os.path.exists("models/model.pkl"):
-        print("Модель не найдена. Обучение...")
+        print("Модель не найдена. Запуск обучения...")
+        os.makedirs("models", exist_ok=True)
         train_and_save_model()
+    else:
+        print("Модель уже обучена.")
