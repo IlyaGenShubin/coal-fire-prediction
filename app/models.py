7715b17280550764b8a83882982241cba0499579
IlyaGenShubin <166981604+IlyaGenShubin@users.noreply.github.com>
2025-11-24 11:47:29 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class PredictionRequest(BaseModel):
+    sklad: int
+    shtabel: int
+    date_str: str
+
+class PredictionResponse(BaseModel):
+    risk: str
+    probability: Optional[float] = None
+    message: Optional[str] = None
